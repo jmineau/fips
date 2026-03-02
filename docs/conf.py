d8cab6a8bad104ccae785b10a551a2a4110dbb1f
James Mineau <jameskmineau@gmail.com>
2026-03-01 19:33:32 -0700

    use dev dependency group

--- docs/conf.py
+++ docs/conf.py
@@ -81,9 +81,6 @@ napoleon_attr_annotations = True
 
 autoclass_content = "class"
 
-# Mock imports for dependencies not available during doc builds
-autodoc_mock_imports = ["stilt", "cartopy", "matplotlib"]
-
 # Autosummary settings
 autosummary_generate = True
 
